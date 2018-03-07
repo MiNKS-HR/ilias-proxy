6b40e5098fa8e3a3b2003115159c720067c1fb7d
ischulz <iliasschulz@yahoo.com>
2018-03-06 19:21:33 -0800

    all five components

--- ilias-proxy/server/index.js
+++ ilias-proxy/server/index.js
@@ -11,27 +11,40 @@ var request = require('request');
 
 app.use('/experience/similar', function(req,res) {
   var newurl = 'http://localhost:3003/experience/similar' + req.url;
-  console.log('CurrentURLSIM',req.url);
   request(newurl).pipe(res);
 });
 
 app.use('/experience/details', function(req,res) {
   var newurl = 'http://localhost:3005/experience/details' + req.url;
-  console.log('CurrentURLDET',req.url);
   request(newurl).pipe(res);
 });
 
 app.use('/host', function(req,res) {
   var newurl = 'http://localhost:3005/host' + req.url;
+  request(newurl).pipe(res);
+});
+
+app.use('/experience/availableDate/', function(req,res) {
+  var newurl = 'http://localhost:3002/experience/availableDate' + req.url;
+  request(newurl).pipe(res);
+});
+
+app.use('/images/', function(req,res) {
+  var newurl = 'http://localhost:3004/images' + req.url;
+  request(newurl).pipe(res);
+});
+
+app.use('/img/', function(req,res) {
+  var newurl = 'http://localhost:3004/img' + req.url;
+  request(newurl).pipe(res);
+});
+
+app.use('/reviews/', function(req,res) {
+  var newurl = 'http://localhost:3001/reviews' + req.url;
   console.log('CurrentURLHOST',req.url);
   request(newurl).pipe(res);
 });
 
-app.use('/reviews', function(req,res) {
-  var newurl = 'http://localhost:3005/host' + req.url;
-  console.log('CurrentURLHOST',req.url);
-  request(newurl).pipe(res);
-});
 
 
 //DOESNT APPEAR TO WORK
