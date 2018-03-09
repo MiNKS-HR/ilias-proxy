f5b64ce589080fec3288fa312b2ff401e3201441
ischulz <iliasschulz@yahoo.com>
2018-03-08 18:49:50 -0800

    changes

--- ilias-proxy/server/index.js
+++ ilias-proxy/server/index.js
@@ -10,12 +10,12 @@ app.use(express.static(path.join(__dirname, '../public/')));
 var request = require('request');
 
 app.use('/similar', function(req,res) {
-  var newurl = 'http://18.219.157.42:80/similar' + req.url;
+  var newurl = 'http://localhost:3003/similar' + req.url;
   request(newurl).pipe(res);
 });
 
 app.use('/details', function(req,res) {
-  var newurl = 'http://18.221.6.243:80/details' + req.url;
+  var newurl = 'http://localhost:3005/details' + req.url;
   request(newurl).pipe(res);
 });
 
