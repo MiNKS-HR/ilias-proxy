ab512193dd3a8a189404319d8019c37e6030c2ce
ischulz <iliasschulz@yahoo.com>
2018-03-09 19:02:47 -0800

    added availability

--- ilias-proxy/server/index.js
+++ ilias-proxy/server/index.js
@@ -2,48 +2,47 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-var proxy = require('express-http-proxy');
 const port = process.env.PORT || 3000;
-
 //app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../public/')));
 var request = require('request');
 
+
+
+app.use('/', express.static(path.join(__dirname, '../public/')));
+app.use('/:id', express.static(path.join(__dirname, '../public/')));
+
+
+
 app.use('/similar', function(req,res) {
-  var newurl = 'http://localhost:3003/similar' + req.url;
+  var newurl = 'http://13.56.28.204:80/similar' + req.url;
   request(newurl).pipe(res);
 });
 
 app.use('/details', function(req,res) {
-  var newurl = 'http://localhost:3005/details' + req.url;
+  var newurl = 'http://54.176.214.18:80/details' + req.url;
   request(newurl).pipe(res);
 });
 
-app.use('/experience/availableDate/', function(req,res) {
-  var newurl = 'http://localhost:3002/experience/availableDate' + req.url;
-  request(newurl).pipe(res);
-});
+// app.use('/experience/availableDate/', function(req,res) {
+//   var newurl = 'http://localhost:3002/experience/availableDate' + req.url;
+//   request(newurl).pipe(res);
+// });
 
-app.use('/images/', function(req,res) {
-  var newurl = 'http://localhost:3004/images' + req.url;
-  request(newurl).pipe(res);
-});
+// app.use('/images/', function(req,res) {
+//   var newurl = 'http://localhost:3004/images' + req.url;
+//   request(newurl).pipe(res);
+// });
 
-app.use('/img/', function(req,res) {
-  var newurl = 'http://localhost:3004/img' + req.url;
-  request(newurl).pipe(res);
-});
+// app.use('/img/', function(req,res) {
+//   var newurl = 'http://localhost:3004/img' + req.url;
+//   request(newurl).pipe(res);
+// });
 
 app.use('/reviews/', function(req,res) {
-  var newurl = 'http://localhost:3001/reviews' + req.url;
+  var newurl = 'http://54.153.115.227:80/reviews' + req.url;
   request(newurl).pipe(res);
 });
 
-
-
-//DOESNT APPEAR TO WORK
-//app.use('*', proxy('https://localhost:3001'));
-//might want to look into it
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`)
